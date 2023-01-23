3f6c2276903f094cd86af47335d08499782d8fe8
marinaferreras <114932704+marinaferreras@users.noreply.github.com>
2023-01-23 16:37:51 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from plotnine import *
+import warnings
+warnings.filterwarnings("ignore")
+data=pd.read_csv('data.csv')
+data_es=data.where(data['Region']=='es').where(data['Position']<=2).dropna()
+gg=ggplot(data_es)+aes(x='Track Name', y='Streams', color='Artist')+geom_point(size=2)+ ggtitle('Canciones más escuchadas en España en 2017')+ theme(axis_text_x  = element_text(angle = 90, hjust = 1))
+ggsave(gg,'plot.jpg')
