1cc59098bd866c5eb54ff300b6c45d6f6f6ac20a
marinaferreras <114932704+marinaferreras@users.noreply.github.com>
2023-01-30 16:39:58 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ import warnings
 warnings.filterwarnings("ignore")
 data=pd.read_csv(sys.argv[1])
 data_es=data.where(data['Region']=='es').where(data['Position']<=2).dropna()
+print ('Number of arguments: %i arguments' % len(sys.argv))
+print ('Argument List:' + str(sys.argv))
 gg=ggplot(data_es)+aes(x='Track Name', y='Streams', color='Artist')+geom_point(size=2)+ ggtitle('Canciones más escuchadas en España en 2017')+ theme(axis_text_x  = element_text(angle = 90, hjust = 1))
 print(gg)
 ggsave(gg,'plot.jpg')
